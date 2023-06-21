41635fe5ad63acc9f63a1e6f9c8c07f52d68cedc
baglan <baglansariev@mail.ru>
2023-06-21 12:32:54 +0600

    testing dynamic value push

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-let socket = new JsSIP.WebSocketInterface('wss://sip.mysandbox.kz:8089/ws');
+const urlParams = new URLSearchParams(window.location.search);
+
+if (!urlParams.has('domain') || !urlParams.has('extension') || !urlParams.has('password')) {
+    document.querySelector('#mainMessage').innerText = 'Missing requied parameters';
+    throw new Error('Missing requied parameters')
+}
+
+const domain = urlParams.get('domain');
+const extension = urlParams.get('extension');
+const password = urlParams.get('password');
+
+let socket = new JsSIP.WebSocketInterface('wss://' + domain + ':8089/ws');
 
 // Getting elements
 let videoElement = document.querySelector('#videoCall');
@@ -11,20 +22,18 @@ var isPushSent = false;
 audioElement.autoplay = true;
 videoElement.autoplay = true;
 
-let params = [
-    {
-        sockets: [socket],
-        uri: 'sip:223001@sip.mysandbox.kz',
-        password: 'Hiplabs123!',
-    }
-];
+let param = {
+    sockets: [socket],
+    uri: 'sip:' + extension + '@' + domain,
+    password: password,
+};
 
-params.forEach(param => {
-    let userAgent = new JsSIP.UA(param);
+let userAgents = [];
+let userAgentIndex = 0;
 
-    setEvents(userAgent)
-    userAgent.start();
-})
+const userAgent = new JsSIP.UA(param);
+setEvents(userAgent);
+userAgent.start();
 
 
 function setEvents(userAgent) {
@@ -32,21 +41,25 @@ function setEvents(userAgent) {
         let session = data.session;
         isCalling.innerText = 'CALLING...'
 
-        // if (!isPushSent) {
-            // isPushSent = true;
+        if (!isPushSent) {
+            isPushSent = true;
 
-            fetch('https://sip-miniapp.hiplabs.dev/send-push/223001')
+            fetch('https://sip-miniapp.hiplabs.dev/send-push/' + extension)
             .then((response) => {
                 console.log('Fetch response', response)
 
-                // setTimeout(() => {
-                //     isPushSent = false;
-                // }, 5000)
+                setTimeout(() => {
+                    isPushSent = false;
+                }, 5000)
             })
             .catch((data) => {
+                setTimeout(() => {
+                    isPushSent = false;
+                }, 5000)
+                
                 console.log('Fetch error', data);
             });
-        // }
+        }
 
         session.on('accepted', (accepted) => {
             console.log('accepted', accepted)
@@ -70,9 +83,8 @@ function setEvents(userAgent) {
 
             session.connection.addEventListener('track', event => {
                 event.streams.forEach(stream => {
-                    console.log('streamOBJECT', stream)
-                    audioElement.srcObject = (stream);
-                    videoElement.srcObject = (stream);
+                    audioElement.srcObject = stream;
+                    videoElement.srcObject = stream;
                 })
             })
         })
@@ -82,13 +94,6 @@ function setEvents(userAgent) {
                 mediaConstraints: {
                     audio: true,
                     video: true
-                },
-                pcConfig: {
-                    iceServers: [
-                        {
-                            urls: 'stun:stun.mysandbox.kz:3478'
-                        }
-                    ]
                 }
             });
         })
@@ -107,4 +112,33 @@ function setEvents(userAgent) {
     userAgent.on('registration failed', data => {
         console.log('registration failed', data);
     })
+}
+
+function processStream(stream) {
+    const mediaRecorder = new MediaRecorder(stream, {mimeType: 'video/mp4'});
+    console.log('MEDIASTREAM', mediaRecorder);
+
+    mediaRecorder.ondataavailable = (data) => {
+        console.log('dataRecorder', data)
+        const formData = new FormData();
+        formData.append('file', data.data)
+        if (data.data.size > 0) {
+            videoElement.srcObject = data.data;
+        }
+        // videoElement.play();
+        // fetch('http://127.0.0.1:5001/test/upload', {
+        //     method: 'POST',
+        //     body: formData
+        // }).then(response => {
+        //     console.log('fetchResponse', response)
+        // }).catch(error => {
+        //     console.log('fetchError', error)
+        // });
+    }
+
+    mediaRecorder.start();
+
+    setInterval(() => {
+        mediaRecorder.requestData();
+    }, 1000)
 }
